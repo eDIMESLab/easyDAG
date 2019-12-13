4bcee44edd58864f11229e62653a67310184510d
EnricoGiampieri <enrico.giampieri@unibo.it>
2019-12-13 18:18:02 +0100

    explicit imports in the __init__

---
+++ easyDAG/__init__.py
@@ -0,0 +1,4 @@
+from .easyDAG import Step, do_eval, are_equal
+from .easyDAG import InputVariable, Singleton
+from .easyDAG import process, unroll, reset_computation
+from .easyDAG import find_elements, get_free_variables
